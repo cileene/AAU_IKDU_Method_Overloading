9acacbcb5d06a154cd0fa4714da9ddb8317e7639
cileene <106276740+cileene@users.noreply.github.com>
2024-10-19 00:36:52 +0200

    Add MethodOverload script and README

--- Assets/Scripts/MethodOverload.cs
+++ Assets/Scripts/MethodOverload.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class MethodOverload : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // The first GettingOlder method increasing age by 1
+    public int GettingOlder(int currentAge)
     {
-        
+        currentAge += 1;
+
+        Debug.Log($"You are aging rapidly and you are now {currentAge} years old. Yikes...");
+
+        return currentAge;
     }
 
-    // Update is called once per frame
-    void Update()
+    // The second GettingOlder method increasing age by the 
+    // length of my ongoing college education
+    public int GettingOlder(int currentAge, int letsGoToCollege)
     {
-        
+        currentAge += letsGoToCollege;
+
+        // Yes this string is a bit long.
+        Debug.Log($"You are aging rapidly and now you chose to go to college for {letsGoToCollege} years?! You are now {currentAge} years old. And without a pension plan.");
+
+        return currentAge;
     }
+
+    void Start()
+    {
+        // Declaring my age
+        int myAge = 33;
+        myAge = GettingOlder(myAge);
+
+        // Now you might argue that random isn't a predefined value
+        // But to that I argue that the value is predefined by being coded as a random value
+        // Ergo, it is predefined value
+        // This was fun, thank you for coming to my TED talk
+        int randomYears = Random.Range(1, 8);
+        myAge = GettingOlder(myAge, randomYears);
+    }
+    // PS. And I am sorry for violating all conventions of effective method and variable naming
 }
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AAU_IKDU_Method_Overloading
- 1. Define a method with one int parameter (currentAge) and test it with Debug.Log - it should  increase age of your player for a single year.  2. Define and test a method  with the same name as previous one , that increases age for a predefined amount of years. Test with Debug.Log for several values, defined with variables.
+
+## Assignment
+
+ 1. Define a method with one int parameter (currentAge) and test it with Debug.Log - it should  increase age of your player for a single year.  
+ 2. Define and test a method  with the same name as previous one , that increases age for a predefined amount of years. Test with Debug.Log for several values, defined with variables.
+
+## Using
+
+- Unity 6 LTS (6000.0.23f1)
